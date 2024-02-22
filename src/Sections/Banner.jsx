dd72ed287eff97afca5e759319b56bd1a5e6e311
Nasiful Islam Nasif <nasifulislamnasif23@gmail.com>
2024-02-23 00:26:48 +0600

    create banner and services section

--- src/Sections/Banner.jsx
+++ src/Sections/Banner.jsx
@@ -1,11 +1,30 @@
 import NavBar from '../Components/NavBar';
 import moduleName from '../assets/homeBg.svg';
+import arrowIcon from '../assets/iconArrow.svg'
+import Services from './Services';
 const Banner = () => {
     return (
-        <div style={{backgroundImage: `url('${moduleName}')`}} className='bg-cover bg-no-repeat md:bg-center lg:bg-top h-[1095px]'>
+        <>
+        <div style={{ backgroundImage: `url('${moduleName}')` }} className='bg-cover bg-no-repeat md:bg-center  h-[1095px]'>
             <NavBar></NavBar>
+            <div className='text-center text-white max-w-[800px] mx-auto'>
+                <h1 className='text-[96px] leading-tight pt-[54px] pb-10'>Navigating the Digital Landscape</h1>
+                <div className=''>
+                    <span className='text-[28px]  px-[45px] py-[15px] bg-[#FFA252] rounded-full'>Welcome to the Sands of Dune!</span>
+                </div>
+                <div className='pt-[250px]'>
+                    <a href="#"><button style={{ boxShadow: "0px 8px 22px 0px rgba(20, 77, 138, 0.30)" }} className="text-[#323232] font-bold bg-white px-[30px] py-[18px] rounded-full mb-12 md:mb-0  hover:scale-105 duration-300">Explore Our Services</button></a>
+                </div>
+                <div className='flex justify-center pt-[18px]'>
+                    <img src={arrowIcon} alt="" />
+                </div>
+            </div>
+
         </div>
+        <Services></Services>
+        </>
     );
 };
 
 export default Banner;
+// 
---
+++ src/Sections/Services.jsx
@@ -0,0 +1,44 @@
+import searchIcon from "../assets/icon-for-services/searchIcon.svg"
+import codeIcon from "../assets/icon-for-services/codeIcon.svg"
+import videoIcon from "../assets/icon-for-services/videoIcon.svg"
+import noteBookIcon from "../assets/icon-for-services/noteIcon.svg"
+import socialIcon from "../assets/icon-for-services/Glyph.svg"
+import dollarIcon from "../assets/icon-for-services/dollarIcon.svg"
+const Services = () => {
+    return (
+        <div className="bg-white py-[50px] md:py-[130px] max-w-[1160px] mx-auto px-4 md:px-10 space-x-8">
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
+                <div className="col-span-1 md:col-span-3 lg:col-span-2 max-w-[390px]">
+                    <h1 className="text-[#0D0D0D] text-[40px]  leading-tight">Our Digital <br /> Marketing Expertise</h1>
+                    <p className="text-[#888] pt-[14px]">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor Lorem ipsum dolor sit</p>
+                </div>
+                <div className="bg-[#F3F3F3] py-[50px] pl-[30px] rounded-3xl">
+                    <img src={searchIcon} alt="" />
+                    <p className="text-[#0D0D0D] pt-[30px] text-[20px]">Search Engine Optimization</p>
+                </div>
+                <div className="bg-[#F3F3F3] py-[50px] pl-[30px] rounded-3xl">
+                    <img src={codeIcon} alt="" />
+                    <p className="text-[#0D0D0D] pt-[30px] text-[20px]">Website design & development</p>
+                </div>
+                <div className="bg-[#F3F3F3] py-[50px] pl-[30px] rounded-3xl">
+                    <img src={videoIcon} alt="" />
+                    <p className="text-[#0D0D0D] pt-[30px] text-[20px]">Video editing & <br /> production</p>
+                </div>
+                <div className="bg-[#F3F3F3] py-[50px] pl-[30px] rounded-3xl">
+                    <img src={noteBookIcon} alt="" />
+                    <p className="text-[#0D0D0D] pt-[30px] text-[20px]">Content <br /> writing</p>
+                </div>
+                <div className="bg-[#F3F3F3] py-[50px] pl-[30px] rounded-3xl">
+                    <img src={socialIcon} alt="" />
+                    <p className="text-[#0D0D0D] pt-[30px] text-[20px]">Social media <br /> marketing</p>
+                </div>
+                <div className="bg-[#F3F3F3] py-[50px] pl-[30px] rounded-3xl">
+                    <img src={dollarIcon} alt="" />
+                    <p className="text-[#0D0D0D] pt-[30px] text-[20px]">Pay per <br /> click</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
